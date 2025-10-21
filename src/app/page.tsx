e0be7ed073e6eba2cec2f5218801148fd518169e
Kristijan Korać <korac.kristijan@gmail.com>
2025-10-21 17:50:16 +0200

    feat: add prototype content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,62 @@
 import Image from "next/image";
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <div className="font-sans items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <div className="max-w-xl lg:mx-auto">
+        <header className="mb-[48px]">
+          <Link href="/" className="flex items-center gap-2 font-medium hover:underline hover:underline-offset-4">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              aria-hidden
+              src="/globe.svg"
+              alt="Globe icon"
+              width={16}
+              height={16}
             />
-            Deploy now
-          </a>
+            Kristijan Korać
+          </Link>
+        </header>
+        <main className="flex flex-col gap-[32px] row-start-2 items-start">
+          <blockquote
+            className="relative before:absolute before:w-[4px] before:top-0 before:bottom-0 before:-left-[16px] before:bg-foreground before:rounded-md">
+            👋<span className="italic ml-1">Speak, friend, and enter.</span>
+          </blockquote>
+          <p className="flex items-center justify-start">
+            I am Kristijan, software engineer and founder from Zagreb, Croatia 🇭🇷.
+          </p>
+          <p>
+            I enjoy crafting digital solutions, user-friendly interfaces and human experiences in a digital world.
+          </p>
+          <h3 className="text-xl font-medium">Mini manifesto</h3>
+          <blockquote
+            className="relative before:absolute before:w-[4px] before:top-0 before:bottom-0 before:-left-[16px] before:bg-foreground before:rounded-md">
+            <span className="italic">Software is for computers. Code is for developers. Interface is for users.</span>
+          </blockquote>
+          <section className="flex flex-col gap-3">
+            <p>
+              Code is essentially a bridge between users and computers. If a bridge is poorly designed on either side, it inevitably collapses.
+            </p>
+            <p>
+              As a software developer, I wish to contribute towards a future that is interconnected with stable, reliable and flexible bridges, where software is used to solve real-world challenges (and to challenge real-world solutions).
+            </p>
+          </section>
+        </main>
+        <footer className="row-start-3 my-10 flex flex-wrap gap-[24px]">
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+            href="https://www.linkedin.com/in/kristijan-korac/"
             target="_blank"
             rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          >Linkedin</a>
+          <a
+            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+            href="https://x.com/korac_kristijan"
+            target="_blank"
+            rel="noopener noreferrer"
+          >X (Twitter)</a>
+        </footer>
+      </div>
     </div>
   );
 }
